e38d3a40ee3ccd0c0f760a771b47e474c535e066
vicmin03 <vicmin03@outlook.com>
2026-08-10 12:16:33 +0100

    Clean up code and add comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState, useEffect, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 import { Navbar } from './components/navbar'
@@ -11,13 +8,12 @@ import song_list from './song_list.json'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // control state of search bar and debouncing text
   const [searchText, setSearchText] = useState("");
   const [debouncedSearchText, setDebouncedSearchText] = useState("")
 
-  // useEffect(() => {
-  //   console.log(searchText)
-  // }, [searchText])
-
+  // add debounce of 5ms so only filters song after user stops typing 
   useEffect(() => {
     const timer = setTimeout(() => {
       setDebouncedSearchText(searchText);
@@ -25,13 +21,15 @@ function App() {
     return () => clearTimeout(timer);
   }, [searchText])
 
+  // handler for user input to search bar; passed down as prop into SearchBar component
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
   }
 
+  // filter songs based on user input to search bar
   const valid_songs = song_list.filter((song) => 
     song.title.toLowerCase().includes(debouncedSearchText.toLowerCase()) || song.eng_title.toLowerCase().includes(debouncedSearchText.toLowerCase()))
-  
+
 
   return (
     <>
@@ -60,8 +58,6 @@ function App() {
           Count is {count}
         </button>
       </section>
-
-
 
     </>
   )
