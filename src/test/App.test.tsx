967bcf721704f035efeb1044ce7dfad22cf109eb
vicmin03 <vicmin03@outlook.com>
2026-08-12 16:22:30 +0100

    Add user event for user interaction testing

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { cleanup, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('App', () => {
