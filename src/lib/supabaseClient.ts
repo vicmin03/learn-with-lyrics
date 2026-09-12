a04822d67e206c5c7407e417a2f997d5e2ee4f6e
vicmin03 <vicmin03@outlook.com>
2026-09-12 13:30:38 +0100

    Fix to pass tests with no env variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-    import.meta.env.VITE_PUBLIC_SUPABASE_URL,
-    import.meta.env.VITE_PUBLIC_SUPABASE_PUBLISHABLE_KEY
-)
+const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const supabase = supabaseUrl && supabaseKey 
+  ? createClient(supabaseUrl, supabaseKey)
+  : null;
