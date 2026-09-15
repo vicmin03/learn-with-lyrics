71c9c7eae00bd9c77b1d6fcf6b285cc18f8e245a
vicmin03 <vicmin03@outlook.com>
2026-09-15 13:55:37 +0100

    Add form validation for login/signup

--- src/components/LogInForm/LogInForm.tsx
+++ src/components/LogInForm/LogInForm.tsx
@@ -1,19 +1,36 @@
 import "./LogInForm.css";
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Input, DialogContentText } from "@mui/material";
+import * as z from "zod";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Input, DialogContentText, CircularProgress } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ErrorText } from "../ErrorText/ErrorText";
 
 interface LogInProps {
     open: boolean,
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+// define validation for login form
+const logInValuesSchema = z.object({
+    email: z.email(),
+    password: z.string().min(6).max(50)
+})
+
+type LogInValues = z.infer<typeof logInValuesSchema>
+
 export function LogInForm ({open, setOpen}: LogInProps ) {
+    // use react-hook-form for validation and easier form management
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<LogInValues>({
+        resolver: zodResolver(logInValuesSchema)
+    })
 
     const handleClose = () => {
         setOpen(false);
     };
 
-    const handleSubmit = () => {
-        console.log("GONNA SUBMIT NOW")
+    const submitForm = (data: LogInValues) => {
+        console.log("GONNA SUBMIT NOW");
+        console.log(data);
     }
 
     return (
@@ -28,28 +45,40 @@ export function LogInForm ({open, setOpen}: LogInProps ) {
                     <DialogContentText>
                         Sign in to your account
                     </DialogContentText>
-                    <form className="log-in-form" onSubmit={handleSubmit} id="log-in-form">
+                    <form className="log-in-form" onSubmit={handleSubmit(submitForm)} id="log-in-form">
                         <div className="form-input-line">
                             <p>Email</p>
                             <Input 
+                                {...register("email")}
                                 fullWidth
                                 id="email-input"
                                 placeholder="Enter your email address"
                             />
+                            <ErrorText msg={errors.email?.message}/>
                         </div>
                         <div className="form-input-line">
                             <p>Password</p>
                             <Input 
+                                {...register("password")}
                                 fullWidth
                                 placeholder="Enter your password"
                                 type="password"/>
+                            <ErrorText msg={errors.password?.message}/>
                         </div>
                     </form>
                 </DialogContent>
                 <DialogActions className="form-buttons">
                     <Button 
                         className="submit-button"
-                        type="submit" form="log-in-form">
+                        type="submit" 
+                        loading={isSubmitting}
+                        loadingIndicator={
+                        <CircularProgress
+                            size={20}
+                            sx={{ color: "white" }}
+                            />
+                        }
+                        form="log-in-form">
                         Log In
                     </Button>
                 </DialogActions>
--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,19 +1,47 @@
 import "../LogInForm/LogInForm.css";
 import "./SignUpForm.css";
 import { Input, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from "@mui/material";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ErrorText } from "../ErrorText/ErrorText";
 
 interface SignInProps {
     open: boolean,
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+const signUpValuesSchema = z.object({
+    email: z.email(),
+    confirmEmail: z.string(),
+    password: z.string().min(6).max(50),
+    confirmPassword: z.string()
+})
+.refine((data) => 
+    data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"]
+})
+.refine((data) => 
+    data.email === data.confirmEmail, {
+        message: "Emails do not match",
+        path: ["confirmEmail"],
+});
+
+type SignUpValues = z.infer<typeof signUpValuesSchema>
+
 export function SignUpForm ({open, setOpen}: SignInProps) {
+    const { register, handleSubmit, formState: { errors, isSubmitting }} = useForm<SignUpValues>({
+        resolver: zodResolver(signUpValuesSchema)
+    })
+
     const handleClose = () => {
         setOpen(false);
     };
 
-    const handleSubmit = () => {
-        console.log("GONNA SIGN UP NOW")
+    const submitForm = (data: SignUpValues) => {
+        console.log("GONNA SIGN UP NOW");
+        console.log(data);
     }
 
     return (
@@ -29,40 +57,48 @@ export function SignUpForm ({open, setOpen}: SignInProps) {
                     <DialogContentText>
                         Create an account to add vocabulary to flashcard decks, request new songs and track your favourite songs/artists
                     </DialogContentText>
-                    <form className="log-in-form" onSubmit={handleSubmit} id="sign-up-form">
+                    <form className="log-in-form" onSubmit={handleSubmit(submitForm)} id="sign-up-form">
                         <div className="form-input-line">
                             <p>Email</p>
                             <Input 
+                                {...register("email")}
                                 fullWidth
                                 id="email-input"
                                 placeholder="Enter your email address"
                             />
+                            <ErrorText msg={errors.email?.message}/>
                         </div>
                         <div className="form-input-line">
                             <p>Confirm Email</p>
                             <Input 
+                                {...register("confirmEmail")}
                                 fullWidth
                                 id="confirm-email-input"
                                 placeholder="Confirm your email address"
                             />
+                            <ErrorText msg={errors.confirmEmail?.message}/>
                         </div>
                         <div className="form-input-line">
                             <p>Password</p>
                             <Input 
+                                {...register("password")}
                                 fullWidth
                                 id="password-input"
                                 placeholder="Enter your password"
                                 type="password"
                             />
+                            <ErrorText msg={errors.password?.message}/>
                         </div>
                         <div className="form-input-line">
                             <p>Confirm Password</p>
                             <Input 
+                                {...register("confirmPassword")}
                                 fullWidth
                                 id="confirm-password-input"
                                 placeholder="Confirm your password"
                                 type="password"
                             />
+                            <ErrorText msg={errors.confirmPassword?.message}/>
                         </div>
                     </form>
                 </DialogContent>
@@ -70,6 +106,7 @@ export function SignUpForm ({open, setOpen}: SignInProps) {
                     <Button
                         className="submit-button"
                         type="submit"
+                        loading={isSubmitting}
                         form="sign-up-form">
                         Sign Up
                     </Button>
